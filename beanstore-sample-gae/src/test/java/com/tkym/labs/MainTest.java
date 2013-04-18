8f289456de89685d10b00fef94542126ff38792c
takayama <tkymism@gmail.com>
2013-04-18 12:57:04 +0900

    fisrt commit.

---
+++ beanstore-sample-gae/src/test/java/com/tkym/labs/MainTest.java
@@ -0,0 +1,12 @@
+package com.tkym.labs;
+
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+	@Test
+	public void testMainJsonCase001(){
+		assertTrue(true);
+	}
+}
