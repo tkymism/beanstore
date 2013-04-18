1400822efe78b2faf690b1aa43aa5af2ef3961a3
takayama <tkymism@gmail.com>
2013-04-18 12:40:18 +0900

    first commit beanstore-sample-gae.

---
+++ beanstore-sample-gae/src/main/java/com/tkym/labs/MainServlet.java
@@ -0,0 +1,10 @@
+package com.tkym.labs;
+
+import javax.servlet.http.HttpServlet;
+
+public class MainServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
